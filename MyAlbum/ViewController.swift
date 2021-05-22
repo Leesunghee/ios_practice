9910a404c693387bad664958e4cb39d6c943ebf4
lee sunghee <realwunz@gmail.com>
2021-05-22 15:15:58 +0900

    Initial Commit

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by lee sunghee on 2021/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
